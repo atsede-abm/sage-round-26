86da3fa135d3a92fb677cc3df8c06bad50acbd51
tsion <tsionyeshetila977@gmail.com>
2025-11-13 13:57:43 +0300

    question 1 commit

---
+++ c++/question 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+     float num1,num2,result;
+	cout<<"Enter your num 1" <<endl;
+	cin>>num1;
+	cout<<"Enter your num 2" <<endl;
+	cin>>num2;
+	result= num1*num2;
+	cout<<"the result is "<<result;
+	return 0;
+}
