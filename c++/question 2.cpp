6c91151facdea2bec8e40c5759e6d5957fb00d42
tsion <tsionyeshetila977@gmail.com>
2025-11-13 14:10:09 +0300

    question 2 commit

---
+++ c++/question 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+	string name ;
+	int age;
+	cout<<"enter your name " <<endl;
+	cin>>name;
+	cout<<"enter your age " <<endl;
+	cin>>age;
+	cout<< "your name is " <<name <<" & your age is "<<age;
+	return 0;
+}
