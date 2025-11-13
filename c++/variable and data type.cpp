c4650cd883c06ee8bd834558be3d28374d1471f0
tsion <tsionyeshetila977@gmail.com>
2025-11-13 12:29:54 +0300

    varial commit

---
+++ c++/variable and data type.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int age =20;
+	age=25;
+	float speed=80.23234;
+	double myspeed=80.236756498;
+	bool istrue=true;
+	char chara ='w';
+	string name = "my name";
+	cout<<age<<endl;
+	cout<<speed<<endl;
+	cout<< myspeed<<endl;
+	cout<< istrue<<endl;
+	cout<<chara<<endl;
+	cout<<name<<endl;
+	return 0;
+}
