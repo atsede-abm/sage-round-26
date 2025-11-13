d9babc37a52f7f0bfd7fb2c55b8afb7d9da3f40a
tsion <tsionyeshetila977@gmail.com>
2025-11-13 13:40:07 +0300

    project commit

---
+++ c++/project.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	double temp,farani;
+	cout <<"Enter your temp "<<endl;
+	cin>>temp;
+	farani =(temp*1.8)+32;
+	cout<<"the temp is "<<farani;
+	return 0;
+}
