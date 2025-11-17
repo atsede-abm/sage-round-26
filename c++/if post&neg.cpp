e91009608203eab3f39f6e777eb05d4d2cb8c86e
tsion <tsionyeshetila977@gmail.com>
2025-11-17 12:34:10 +0300

    if pos &nega commit

---
+++ c++/if post&neg.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int num;
+	cout<<"enter your num"<<endl;
+	cin>>num;
+	if(num>0){
+		cout<<"your number is postive"<<endl;}
+	else if(num==0){
+	 cout<<"your number is 0";}
+				
+		else{
+	cout<<"your number is negative";
+		}
+		return 0;
+	}
+	
