917adcbbf628c818fc38e4321fbcd7a5b84a9049
tsion <tsionyeshetila977@gmail.com>
2025-11-17 12:07:33 +0300

    switch commit

---
+++ c++/switch.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int day =4;
+	switch(day){
+	case 1:
+	cout<<"monday"<<endl;
+	break;
+	case 2:
+	  cout<<"tusday"<<endl;
+	break;
+	case 3:
+	cout<<"wendsday"<<endl;
+	break;
+	default:
+		cout<<"no day";
+	return 0;
+}
+}
