deb6d79df1c45da293b82cfe7f56c28bf49b0d9d
tsion <tsionyeshetila977@gmail.com>
2025-11-17 11:50:31 +0300

    if else if else commit

---
+++ c++/if else if else condition.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int age=16;
+	if(age>=18){
+	cout<<"adult";
+	}else if(age>=15)
+	{
+		cout<<"treen";}
+		
+		else
+
+	{
+		cout<< "child";	
+	}
+
+	
+	return 0;
+	
+}
