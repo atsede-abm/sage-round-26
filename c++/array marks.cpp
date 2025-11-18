26b28fef57027dd1908787e4a3cd3e5db8323c99
tsion <tsionyeshetila977@gmail.com>
2025-11-18 12:10:02 +0300

    array loop marks commit

---
+++ c++/array marks.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+ int marks[5];
+ cout<<"enter 5 marks"<<endl;
+ for(int i=0;i<5;i++){
+ 	cin>>marks[i];
+	 }
+	 cout<<"you entered";
+	 
+	 for(int i=0;i<5;i++){
+	 cout<<marks[i]<<endl;
+	 }
+	 
+	 
+ 
+ 
+ 
+ return 0;
+}
