3998f1a9a26aa4ea539d3f22308b2dc2068040e0
tsion <tsionyeshetila977@gmail.com>
2025-11-18 14:02:27 +0300

    sum array commit

---
+++ c++/sum array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int num1[3]={10,20,30};
+	int num2[3]={50,70,90};
+	int num3[3]={15,25,35};
+	
+	int sum1,sum2,sum3;
+	
+	for(int i=0;i<3;i++){
+	
+		sum1=num1[i]+sum1;
+}
+	for(int a=0;a<3;a++){
+		sum2=num2[a]+sum2;
+}
+	for(int b=0;b<3;b++){
+		sum3=num3[b]+sum3;
+	
+		
+	}
+	cout<<sum1<<endl;
+	cout<<sum2<<endl;
+	cout<<sum3<<endl;
+}
