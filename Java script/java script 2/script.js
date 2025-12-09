2d6c87abbfde0077c4bf3a5c006fcab186cb96cd
computer <computer@gmail.com>
2025-12-09 08:15:17 -0300

    javascripit query commit

--- Java script/java script 2/script.js
+++ Java script/java script 2/script.js
@@ -76,3 +76,96 @@ console.log(zz)
 let age=12
 console.log(age??"age not found")
 
+
+let numbers= [1,2,3,45,67,89,362,125];
+
+for(let number of numbers){
+    console.log('the number' ,": ",[number])
+}
+for(let key in Person){
+
+    console.log(key,":",Person[key])
+}
+let day= 8;
+
+switch(day){
+  case 1:
+    console.log("Mon");
+    break;
+    case 2:
+        console.log("Tus");
+        break;
+    case 3:
+        console.log("Wend");
+        break;
+    case 4:
+        console.log("Thu");
+        break;
+    case 5:
+        console.log("Fri");
+        break;
+    case 6:
+        console.log("Sat");
+        break;
+    case 7:
+        console.log("Sun");
+    break;
+    default: 
+    console.log("the day not found")
+        
+}
+let i=0;
+while(i<5){
+    console.log("while loop :",i)
+    i++;
+}
+for(let j=0; j<5;j++){
+    console.log("for loop :",j)
+
+}
+const students=[
+{id: 1, name:"Elshaday", Gender: "Male",score:[99,98,97]},
+{id: 2, name:"Tselote", Gender: "Male",score:[99,98,97]},
+{id: 3, name:"Israel", Gender: "Male",score:[99,98,97]},
+{id: 4, name:"Mesay", Gender: "Male",score:[99,98,97]},
+{id: 5, name:"Atsede", Gender: "Female",score:[99,98,97]},
+{id: 6, name:"Lidya", Gender: "Female",score:[99,98,97]},
+]
+console.log(students[5].name)
+students[5].name="Fikadu";
+console.log(students[5].name)
+ 
+students.forEach(function(student){
+
+    console.log(student.Gender);
+
+    student.score.forEach((s)=>{
+        console.log(s);
+    })
+ })
+ const h1=document.querySelector(".first-class")
+
+ h1.style.color='red'
+
+//   h1.className="Test"
+
+ const newDiv=document.createElement('div');
+ newDiv.innerText= 'newdiv from javascript';
+ document.body.appendChild(newDiv)
+
+//  document.getElementById('btn') .addEventListener('click',(e)=>{
+//     h1.style.color="blue";
+//  });
+$('#btn').click(function(){
+
+    h1.style.color='blue';
+})
+// $('#btn').hide()
+$('#btn').slideUp()
+
+
+$('#Box').css('width','200px')
+.css('height','200px')
+ .css('background-color','hotpink')
+ .slideUp(2000)
+ .slideDown(2000)
