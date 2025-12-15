1aa741c7489dc2a245e8f7a265721482ac7df647
computer <computer@gmail.com>
2025-12-15 08:20:50 -0300

    react cbe commit

--- Frontend/zemen/src/App.js
+++ Frontend/zemen/src/App.js
@@ -8,6 +8,7 @@ function App() {
   <h2> Test </h2>
   <Button name ="dowunload"/>
   <Button name="test"/>
+  <div style={{'display':'flex'}}>
  <Card img ="https://th.bing.com/th/id/R.d57e0b4de9d0e98cde32ddd5832a3cec?rik=w5bInqRcE9zqkA&riu=http%3a%2f%2fsylvia0333.s.y.pic.centerblog.net%2fbelles-images-belle-image-big-jpgr.jpg&ehk=nJLEgC2rX67yGQspytqFzQ4c9DDiXRwDbrkCCssWqgI%3d&risl=&pid=ImgRaw&r=0"
    title="title 1"
    desc="SponsoredTransform photos using image editing tools. Creative features to explore and use. Image editing can help enhance photos. Experience modern technology and creative features.
@@ -23,6 +24,7 @@ Amenities: AI Video Generation, Image Creation, Thumbnail Design, Product Demo V
    desc="SponsoredTransform photos using image editing tools. Creative features to explore and use. Image editing can help enhance photos. Experience modern technology and creative features.
 Amenities: AI Video Generation, Image Creation, Thumbnail Design, Product Demo Videos"
  />
+ </div>
   </div>
 }
 export default App;
