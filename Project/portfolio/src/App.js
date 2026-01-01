b93c0e5d890f234d48c2fa75f9964451dc39adf9
atsede-abm <atsedeb650@gmail.com>
2026-01-01 14:02:01 +0300

    portfolio commit

--- Project/portfolio/src/App.js
+++ Project/portfolio/src/App.js
@@ -1,28 +1,25 @@
 import React from "react"
 
-import Profile from'./components/Profile'
-import Skills from'./components/Skills'
-import Projects from'./components/Projects'
-import Header from'./components/Header'
-import Contact from'./components/Contact'
+import Profile from './components/Profile'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import Header from './components/Header'
+import Contact from './components/Contact'
 
 import './App.css'
 
- function App(){
-  return(
-    <div className='container'>
-     <Header/>
-        <Profile/>
-        <Skills/>
-        
-        <Projects/>
-        <Contact/>
-        
-        
+function App() {
+  return (
+    <div>
+      <div className='container'>
+        <Header />
+        <Profile />
+        <Skills />
 
-     
+        <Projects />
+      </div>
+      <Contact />
     </div>
   )
-  }
+}
 export default App;
- 
