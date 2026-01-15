9e460c6fe48e948db2efab8d84749ca3af09839d
atsede-abm <atsedeb650@gmail.com>
2026-01-15 14:12:50 +0300

    bruno commit

--- Backend/Routing/index.js
+++ Backend/Routing/index.js
@@ -1,11 +1,62 @@
 const express= require('express')
-const books=require('./data/books')
+let books=require('./data/books')
 
 const app=express()
+app.use(express.json())
+app.use((req,res,next)=>{
+    if(req.query.key !=10){
+        res.status(201).json({
+            success:'false',
+            message:'key not found!'
+        })
+    } else{
+        next()
+    }
+})
 
 app.get('/books',(req,res)=>{
     res.status(200). json(books)
 })
+  
+
+// body
+app.post("/books/",(req,res)=>{
+    const body =req.body
+
+    const id=books.length+1
+    body.id=id
+    console.log(body)
+    books.push(body)
+    res.status(201).json({
+            success: "true",
+            message: "book created"
+        })
+})
+app.delete("/books/:id",(req,res)=>{
+    const id =req.params.id
+    books=books.filter((book)=>book.id !=id)
+    res.status(200).json({
+        success: "true",
+            message: "book deleted"
+    })
+})
+app.put('/books/:id',(req,res)=>{
+     const body =req.body
+    const id =req.params.id
+    books=books.map((book)=>{
+        if(book.id ==id){
+
+            return {...book,...body}
+        }else{
+            return book
+        }
+    })
+    res.status(200).json({
+        success: "true",
+            message: "book Edited!"
+    })
+})
+
 // query
 app.get("/books/search",(req,res)=>{
     const q =req.query.q.toLowerCase()
